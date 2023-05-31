ac900ba00b69117538afbae473a7b2bd2e2088c9
M.Pac <pacifiquemurangwa001@gmail.com>
2023-05-31 19:12:30 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 function App() {
-  return (
-    <div className="App">
-    </div>
+  const [currentPickupLine, setCurrentPickupLine] = useState(
+    " I never believed in love at first sight, but that was before I saw you."
   );
+
+  useEffect(() => {
+    (async function getPickUpLine() {
+      let response = await axios.get("https://vinuxd.vercel.app/api/pickup");
+      setCurrentPickupLine(response.json()["pickup"]);
+    })();
+  }, []);
+
+  return <div className="App">{currentPickupLine}</div>;
 }
 
 export default App;

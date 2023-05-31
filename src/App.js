214a9c41c5c8e02e8b14493129f6a1dc866997da
M.Pac <pacifiquemurangwa001@gmail.com>
2023-05-31 19:45:36 +0200

    feat: added basic functionalities

--- src/App.js
+++ src/App.js
@@ -1,20 +1,41 @@
-import "./App.css";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
-import { useEffect, useState } from "react";
+import "./App.css";
+import pickupLinesLogo from "./images/k.png";
 
 function App() {
-  const [currentPickupLine, setCurrentPickupLine] = useState(
-    " I never believed in love at first sight, but that was before I saw you."
-  );
+  const [currentPickupLine, setCurrentPickupLine] = useState("");
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
-    (async function getPickUpLine() {
-      let response = await axios.get("https://vinuxd.vercel.app/api/pickup");
-      setCurrentPickupLine(response.json()["pickup"]);
-    })();
+    getPickUpLine();
   }, []);
 
-  return <div className="App">{currentPickupLine}</div>;
+  const getPickUpLine = async () => {
+    try {
+      setLoading(true);
+      const response = await axios.get("https://vinuxd.vercel.app/api/pickup");
+      setCurrentPickupLine(response.json()["pickup"]);
+      setLoading(false);
+    } catch (error) {
+      console.error("Error fetching pickup line:", error);
+      setLoading(false);
+    }
+  };
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={pickupLinesLogo} alt="Pickup Lines" className="logo-image" />
+        <div className="pickup-line-box">
+          <p className="pickup-line-text">{currentPickupLine}</p>
+        </div>
+        <button className="next-button" onClick={getPickUpLine} disabled={loading}>
+          {loading ? "Loading..." : "Next"}
+        </button>
+      </header>
+    </div>
+  );
 }
 
 export default App;
